b6865a0c3bf333fbed993dfd5a7633c8627271c2
K-ous Nguyễn <88382943+nguyenvantruong0905@users.noreply.github.com>
2021-09-07 16:48:53 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 
 var jwt = localStorage.getItem("jwt");
 if (jwt != null) {
-  window.location.href = './a.html'
+  window.location.href = './index.html'
 }
 
 function login() {
@@ -27,7 +27,7 @@ function login() {
           confirmButtonText: 'OK'
         }).then((result) => {
           if (result.isConfirmed) {
-            window.location.href = './main.html';
+            window.location.href = './index.html';
           }
         });
       } else {
